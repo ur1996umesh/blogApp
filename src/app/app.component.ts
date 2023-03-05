4566f510062af55f4030b69b72cfbbf0465a811f
Umesh Gajjala <umeshreddy1996.ur@gmail.com>
2023-03-05 20:24:57 +0530

    branch2

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,14 @@
-import { Component,Oninit } from '@angular/core';
+import { Component,OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent implements Oninit{
+export class AppComponent implements OnInit{
   title = 'blogApp';
   ngOnInit(){
-  console.log('yes');
+  console.log('done');
     return;
   }
 }
